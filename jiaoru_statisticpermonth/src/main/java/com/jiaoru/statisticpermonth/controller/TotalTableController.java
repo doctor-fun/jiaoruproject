5f9934b6081c3750da1144823f9f207d996bc6db
chenfang <fxf432@163.com>
2019-03-09 23:59:32 +0800

    3.9

--- jiaoru_statisticpermonth/src/main/java/com/jiaoru/statisticpermonth/controller/TotalTableController.java
+++ jiaoru_statisticpermonth/src/main/java/com/jiaoru/statisticpermonth/controller/TotalTableController.java
@@ -1,5 +1,6 @@
 package com.jiaoru.statisticpermonth.controller;
 
+import com.jiaoru.statisticpermonth.pojo.StatisticsTrainer;
 import com.jiaoru.statisticpermonth.pojo.TotalTableClasshour;
 import com.jiaoru.statisticpermonth.service.TotalTableClasshourService;
 import entity.Result;
@@ -17,18 +18,57 @@ public class TotalTableController {
     private TotalTableClasshourService totalTableClasshourService;
 
     /**
-     *
+     *查看全部
      * @return
      */
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll(){
         return new Result(true, StatusCode.OK,"查询成功",totalTableClasshourService.findAll());
     }
+
+    /**
+     * 条件查询
+     * @param totalTableClasshour
+     * @return
+     */
     @RequestMapping(value="/search",method = RequestMethod.POST)
     public Result findSearch(@RequestBody TotalTableClasshour totalTableClasshour){
         List<TotalTableClasshour> list=totalTableClasshourService.findSearchNoPage(totalTableClasshour);
         return new Result(true,StatusCode.OK,"查询成功了?",list);
     }
+
+    /**
+     *增加记录
+     * @param totalTableClasshour
+     * @return
+     */
+    @PostMapping
+    public Result add(@RequestBody TotalTableClasshour totalTableClasshour){
+        totalTableClasshourService.add(totalTableClasshour);
+        return new Result(true,StatusCode.OK,"添加成功");
+    }
+    /**
+     * 根据id修改行记录
+     * @param totalTableClasshour
+     */
+    @RequestMapping(value="/{id}",method= RequestMethod.PUT)
+    public Result update(@RequestBody TotalTableClasshour totalTableClasshour, @PathVariable String id ){
+        totalTableClasshour.setId(id);
+        totalTableClasshourService.update(totalTableClasshour);
+        return new Result(true,StatusCode.OK,"修改成功");
+    }
+    /**
+     * 根据id删除
+     * @param id
+     */
+    @RequestMapping(value="/{id}",method= RequestMethod.DELETE)
+    public Result delete(@PathVariable String id ){
+        totalTableClasshourService.deleteById(id);
+        return new Result(true,StatusCode.OK,"删除成功");
+    }
+
+
+
 
     
 
