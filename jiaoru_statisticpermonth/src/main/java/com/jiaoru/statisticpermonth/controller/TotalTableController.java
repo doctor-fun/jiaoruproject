e3b2efc08b3d3f0ff4bad9505d82a83d0bc4d19f
chenfang <fxf432@163.com>
2019-03-09 23:41:11 +0800

    3.9

--- jiaoru_statisticpermonth/src/main/java/com/jiaoru/statisticpermonth/controller/TotalTableController.java
+++ jiaoru_statisticpermonth/src/main/java/com/jiaoru/statisticpermonth/controller/TotalTableController.java
@@ -1,11 +1,13 @@
 package com.jiaoru.statisticpermonth.controller;
 
+import com.jiaoru.statisticpermonth.pojo.TotalTableClasshour;
 import com.jiaoru.statisticpermonth.service.TotalTableClasshourService;
 import entity.Result;
 import entity.StatusCode;
-import org.apache.logging.log4j.message.ReusableMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -21,6 +23,11 @@ public class TotalTableController {
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll(){
         return new Result(true, StatusCode.OK,"查询成功",totalTableClasshourService.findAll());
+    }
+    @RequestMapping(value="/search",method = RequestMethod.POST)
+    public Result findSearch(@RequestBody TotalTableClasshour totalTableClasshour){
+        List<TotalTableClasshour> list=totalTableClasshourService.findSearchNoPage(totalTableClasshour);
+        return new Result(true,StatusCode.OK,"查询成功了?",list);
     }
 
     
--- jiaoru_statisticpermonth/src/main/java/com/jiaoru/statisticpermonth/service/TotalTableClasshourService.java
+++ jiaoru_statisticpermonth/src/main/java/com/jiaoru/statisticpermonth/service/TotalTableClasshourService.java
@@ -1,12 +1,19 @@
 package com.jiaoru.statisticpermonth.service;
 
 import com.jiaoru.statisticpermonth.dao.TotalTableClasshourDao;
+
 import com.jiaoru.statisticpermonth.pojo.TotalTableClasshour;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import util.IdWorker;
 
-import javax.xml.ws.Action;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,6 +22,39 @@ public class TotalTableClasshourService {
     private TotalTableClasshourDao totalTableClasshourDao;
     @Autowired
     private IdWorker idWorker;
+
+    /**
+     *
+     * @param totalTableClasshour
+     * @return
+     */
+    public  List<TotalTableClasshour> findSearchNoPage(TotalTableClasshour totalTableClasshour) {
+        List<TotalTableClasshour> list;
+        list=totalTableClasshourDao.findAll(new Specification<TotalTableClasshour>() {
+            /**
+             * 根对象，也就是要把条件封装到哪个对象中，where 类名=statisticsTrainer.get()条件
+             * @param root
+             * @param criteriaQuery
+             * @param criteriaBuilder 用来封装条件对象，如果直接返回null，则无条件
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<TotalTableClasshour> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> list =new ArrayList<>();
+
+                if(totalTableClasshour.getTrainername()!=null && !"".equals(totalTableClasshour.getTrainername())){
+                    Predicate predicate = criteriaBuilder.like(root.get("trainername").as(String.class), "%" + totalTableClasshour.getTrainername() + "%");
+                    //相当于//where statisticTrainer类中 类似于 "%中文字符%"
+                    list.add(predicate);
+                }
+                Predicate[] parr=new Predicate[list.size()];
+                parr=list.toArray(parr);//将list的内容传给数组parr
+                return criteriaBuilder.and(parr);
+            }
+        });
+
+        return list;
+    }
 
     /**
      *
